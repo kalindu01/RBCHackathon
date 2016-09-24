002d8acee3f47bdb87a14c572fc994461038e254
janamanoharan <jana.manoharan96@gmail.com>
2016-09-24 17:03:56 -0400

    added stuff

--- WebApp/js/express.js
+++ WebApp/js/express.js
@@ -1,24 +1,44 @@
 var express = require('express');
+var fs = require("fs");
+var readline = require('readline');
+
 var app = express();
 
 app.get('/', function (req, res) {
-   console.log("Got a GET request for the homepage");
-   res.sendFile("//Users/JanaMano/Desktop/RBCHackathon/RBCHackathon/WebApp/index.html");
+   res.sendFile("/Users/JanaMano/Desktop/RBCHackathon/RBCHackathon/WebApp/index.html");
 })
 
-app.get('/process_get', function (req, res) {
-   // Prepare output in JSON format
-   response = {
+app.get('/expand_investment', function (req, res) {
+      index = req.query.clickedIndex{
       first_name:req.query.first_name,
       last_name:req.query.last_name
    };
-   console.log(response);
-   res.end(JSON.stringify(response));
 })
+
+app.get('/list_investments', function (req, res) {
+   // open hte investments file
+   var rd = readline.createInterface({
+    input: fs.createReadStream('../investment_request.csv'),
+    output: process.stdout,
+    terminal: false
+   });
+   // create a 2d array for all the arrays
+   var investments_array = [];
+   // read one line at a line
+   rd.on('line', function(line) {
+      //add it to the array
+      var current_row = line.split("|");
+      for (var i = 0; i < current_row.length; i++) {
+         var row_in_json = {}
+         investments_array.push(current_row);
+      }
+   });
+   res.send(investments_array);
+})
+
 var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
-   
    console.log("Example app listening at http://%s:%s", host, port)
-})
 
+})
