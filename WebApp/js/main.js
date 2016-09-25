d37ae869c09d0b71949532f1cff59eda32337519
sakshaat_dc <sakshaat_dc@hotmail.com>
2016-09-25 01:22:47 -0400

    main.js updated

--- WebApp/js/main.js
+++ WebApp/js/main.js
@@ -36,4 +36,10 @@ $(document).ready(function() {
     });
 });
 
-
+var app = angular.module('investmentsTable', []);
+app.controller('investmentsCtrl', function($scope, $http) {
+    $http.get("http://127.0.0.1:8081/get_investments").then(function (response) {
+        console.log("RECORDS " + response.data.records);
+        $scope.names = response.data.records;
+    });
+});
